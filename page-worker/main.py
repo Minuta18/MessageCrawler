2471281d3a1a5d73d39d9c14f2d84d141804ceac
Minuta18 <ig.samsonov10@yandex.ru>
2024-05-22 12:58:24 +0300

    page worker

--- page-worker/main.py
+++ page-worker/main.py
@@ -1,22 +1,23 @@
 import worker_library
 import os
 import httpx
+import bs4
 
 connection = worker_library.RabbitConnection(
     f'ampq://{ os.environ.get('RABBIT_USER') }:' + 
     f'{ os.environ.get('RABBIT_PASS') }@'
     f'{ os.environ.get('RABBIT_HOST') }:' +
     f'{ os.environ.get('RABBIT_PORT') }/',
-    'links'
+    'pages'
 )
 
-def get_link_source(ch, method, properties, body):
-    link = body
-    response = httpx.get(link)
+def get_links_by_source(ch, method, properties, body):
+    page = body
+    soup = bs4.BeautifulSoup(page, 'html.parser')
     
-    if response.status_code == 200:
-        connection.send_message('pages', response.content.decode('utf-8'))
-
+    for link in soup.find_all('a'):
+        connection.send_message('links', link.get('href'))
+    
 if __name__ == '__main__':
-    connection.set_on_receive_callback(get_link_source)
+    connection.set_on_receive_callback(get_links_by_source)
     connection.run()
--- update_lib.sh
+++ update_lib.sh
@@ -1,1 +1,2 @@
 cp -r ./worker-library ./link-crawler
+cp -r ./worker-library ./page-worker
